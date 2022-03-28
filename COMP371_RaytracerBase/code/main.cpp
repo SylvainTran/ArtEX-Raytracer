d17136ea7734f98984340f7921691c3de4a56043
Sylvain Tran <sylvain.serey.tran@protonmail.com>
2022-03-28 17:20:18 -0400

    Matched main.cpp and added student json in src/

--- COMP371_RaytracerBase/code/main.cpp
+++ COMP371_RaytracerBase/code/main.cpp
@@ -1,13 +1,3 @@
-////////////////////////
-/**
- * 
- * Author: Sylvain Tran
- * Program Description: The driver for the raytracer.
- * Date Created: Tue, Jan. 11th, 2022
- * Date Last Updated: Tue, Jan. 11th, 2022
- * 
- */
-////////////////////////
 #ifdef COURSE_SOLUTION
 #include "src/RayTracer.h"
 #endif
@@ -16,61 +6,71 @@
 #include "src/RayTracer.h"
 #endif
 
-// Standard
+
 #include <iostream>
 #include <string>
-#include <memory>
-using std::cout;
-using std::endl;
+#include <Eigen/Core>
+#include <Eigen/Dense>
 
-// External
 #include "external/json.hpp"
 #include "external/simpleppm.h"
 
-// My code
+
 using namespace std;
 
 int test_eigen();
 int test_save_ppm();
 int test_json(nlohmann::json& j);
-
-void runDummyRaytracer() {
-    cout<<"Invalid number of arguments"<<endl;
-    cout<<"Usage: ./raytracer [scene] "<<endl;
-    cout<<"Run sanity checks"<<endl;    
-    test_eigen();
-    test_save_ppm();
-};
-
+    
 int main(int argc, char* argv[])
 {
     if(argc!=2){
-        runDummyRaytracer();
+        cout<<"Invalid number of arguments"<<endl;
+        cout<<"Usage: ./raytracer [scene] "<<endl;
+        cout<<"Run sanity checks"<<endl;
+        
+        test_eigen();
+        test_save_ppm();
+        
     } else {
+        
+        cout<<"Scene: "<<argv[1]<<endl;
+        
         std::ifstream t(argv[1]);
-        if(!t) {
-          cout<<"File "<<argv[1]<<" does not exist!"<<endl; 
-          return -1;
+        if(!t){
+            cout<<"File "<<argv[1]<<" does not exist!"<<endl;
+            return -1;
         }
-
+        
         std::stringstream buffer;
-        buffer<<t.rdbuf();
+        buffer << t.rdbuf();
+        
         nlohmann::json j = nlohmann::json::parse(buffer.str());
-        cout<<"Parsed successfully"<<endl;
-
+        cout<<"Parsed successfuly"<<endl;
+        
 #ifdef COURSE_SOLUTION
+        srand(234);
         cout<<"Running course solution"<<endl;
+        time_t tstart, tend;
+        tstart = time(0);
         RT371::RayTracer<RT371::Kernelf> rt(j);
         cout<<"Running!"<<endl;
         rt.run();
+        tend = time(0);
+        cout << "It took "<< difftime(tend, tstart) <<" second(s)."<< endl;
 #else
         
 #ifdef STUDENT_SOLUTION
         cout<<"Running student solution"<<endl;
-        RayTracer rt = RayTracer(j);
+
+        time_t tstart, tend;
+        tstart = time(0);
+        RayTracer rt(j);
         rt.run();
+        tend = time(0);
+        cout << "It took "<< difftime(tend, tstart) <<" second(s)."<< endl;
 #else
-        // Given code - a bunch of test functions to showcase the funcitonality
+        // GIven code - a bunch of test functions to showcase the funcitonality
         test_eigen();
         test_save_ppm();
         
@@ -80,11 +80,13 @@ int main(int argc, char* argv[])
             cout<<"Could not load file!"<<endl;
         }
 #endif
-
-
-
+        
+        
 #endif
     }
+  
+    
+    
     return 0;
 }
 
